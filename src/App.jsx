d7167b65666337bc38c89fa35767e26867e88cbe
Dev-Harsh0218 <mailharsh0218@gmail.com>
2025-04-04 02:13:58 +0530

    added to github

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import CustomCursor from "./components/CustomCursor";
+function App() {
+  return (
+    <div aclassName="relative">
+      <CustomCursor />
+    </div>
+  )
+}
+
+export default App
